8508534a70109379c3bfbf128df94afa2b434b0d
Simon <simonkjaer449@gmail.com>
2021-02-04 14:12:49 +0100

    Overloads til funktioner undtaget division

--- Lommeregner/Calculator.cs
+++ Lommeregner/Calculator.cs
@@ -7,6 +7,7 @@ namespace Lommeregner
     public class Calculator1
     {
         public double Accumulator { get; private set; }
+
         public double Add(double a, double b)
         {
             Accumulator = a + b;
@@ -14,24 +15,62 @@ namespace Lommeregner
             return Accumulator;
         }
 
+        public double Add(double addend)
+        {
+            double result = Accumulator + addend;
+
+            Accumulator = result;
+
+            return result;
+        }
+
+
         public double Subtract(double a, double b)
         {
             Accumulator = a - b;
             return Accumulator;
 
         }
+
+        public double Subtract(double subtractor)
+        {
+            double result = Accumulator + subtractor;
+
+            Accumulator = result;
+
+            return result;
+        }
+
         public double Multiply(double a, double b)
         {
             Accumulator = a * b;
             return Accumulator;
 
         }
+
+        public double Multiply(double multiplier)
+        {
+            double result = Accumulator + multiplier;
+
+            Accumulator = result;
+
+            return result;
+        }
+
         public double Power(double x, double exp)
         {
             Accumulator = Math.Pow(x, exp);
             return Accumulator;
 
         }
-        
+
+        public double Power(double exponent)
+        {
+            double result = Math.Pow(Accumulator, exponent);
+
+            Accumulator = result;
+
+            return result;
+        }
     }
 }
--- Lommeregner/Program.cs
+++ Lommeregner/Program.cs
@@ -8,9 +8,13 @@ namespace Lommeregner
         {
             Console.WriteLine("Hello World!");
             Calculator1 a = new Calculator1();
-            Console.WriteLine($"3 opløftet i 4. er {a.Power(3,4)}");
+            //Console.WriteLine($"3 opløftet i 4. er {a.Power(3,4)}");
 
             Console.WriteLine($"Nemt {a.Accumulator + a.Add(1, 2)}");
+
+            Console.WriteLine($"Nemt {a.Add(2)}");
+
+            Console.WriteLine($"Nemt {a.Add(2)}");
         }
     }
 }
