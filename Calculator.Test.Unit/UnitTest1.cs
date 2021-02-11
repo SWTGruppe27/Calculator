c5f82f68f24903d35cbcca83d1eeef2134b17e1a
Kathrine <61585423+Kat1606@users.noreply.github.com>
2021-02-11 13:02:28 +0100

    Kommenteret unittest

--- Calculator.Test.Unit/UnitTest1.cs
+++ Calculator.Test.Unit/UnitTest1.cs
@@ -16,6 +16,91 @@ namespace Calculator.Test.Unit
             uut = new Calculator();
         }
 
+        //*** Add ***
+        [Test]
+        public void Add_Integers_SumIsCorrect()
+        {
+            //Act
+            var result = uut.Add(13, 1);
+            //Assert
+            Assert.That(result, Is.EqualTo(14));
+        }
+
+        [Test]
+        public void Add_Zero_SumIsCorrect()
+        {
+            //Act
+            var result = uut.Add(0, 0);
+            //Assert
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Add_ZeroAnd10_SumIsCorrect()
+        {
+            //Act
+            var result = uut.Add(10, 0);
+            //Assert
+            Assert.That(result, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void Add_Decimal_SumIsCorrect()
+        {
+            //Act
+            var result = uut.Add(10.1200, 11.3300);
+            //Assert
+            Assert.That(result, Is.EqualTo(21.45));
+        }
+
+        [TestCase(1, 2, 3)]
+        [TestCase(3, 3, 6)]
+        [TestCase(11.11, 11.11, 22.22)]
+        public void Add_Test3cases_SumIsCorrect(double n, double d, double q)
+        {
+            var result = uut.Add(n, d);
+            Assert.That(q, Is.EqualTo(result));
+        }
+
+        [TestCase(2, 4)]
+        [TestCase(5, 10)]
+        [TestCase(0, 0)]
+        public void Add_overloadedFunction_SumIsCorrect(double a, double result)
+        {
+            uut.Add(a);
+            Assert.That(uut.Add(a), Is.EqualTo(result));
+        }
+
+
+        //*** Subtract ***
+        [TestCase(4, 2, 2)]
+        [TestCase(-4, -3, -1)]
+        [TestCase(-2, 5, -7)]
+        public void Subtract_Test3Cases_SumIsCorrect(double a, double b, double result)
+        {
+            Assert.That(uut.Subtract(a, b), Is.EqualTo(result));
+        }
+
+        [TestCase(2, 0)]
+        [TestCase(-3, 5)]
+        [TestCase(5, -3)]
+        public void Subtract_Overloaded_SumIsCorrect(double b, double result)
+        {
+            uut.Subtract(4, 2);
+            Assert.That(uut.Subtract(b), Is.EqualTo(result));
+        }
+
+        [TestCase(2, -4)]
+        [TestCase(5, -10)]
+        [TestCase(0, 0)]
+        public void Subtract_overloadedFunction_SumIsCorrect(double a, double result)
+        {
+            uut.Subtract(a);
+            Assert.That(uut.Subtract(a), Is.EqualTo(result));
+        }
+
+
+        //*** Multiply ***
         [Test]
         //method, scenario and expected result
         public void Multiply_3Times3_ProductIsCorrect()
@@ -53,68 +138,17 @@ namespace Calculator.Test.Unit
             Assert.That(result, Is.EqualTo(3*3.14));
         }
 
-        [Test]
-        public void Add_Integers_SumIsCorrect()
-        {
-            //Act
-            var result = uut.Add(13, 1);
-            //Assert
-            Assert.That(result, Is.EqualTo(14));
-        }
-
-        [Test]
-        public void Add_Zero_SumIsCorrect()
-        {
-            //Act
-            var result = uut.Add(0, 0);
-            //Assert
-            Assert.That(result, Is.EqualTo(0));
-        }
-
-        [Test]
-        public void Add_ZeroAnd10_SumIsCorrect()
-        {
-            //Act
-            var result = uut.Add(10, 0);
-            //Assert
-            Assert.That(result, Is.EqualTo(10));
-        }
-
-        [Test]
-        public void Add_Decimal_SumIsCorrect()
-        {
-            //Act
-            var result = uut.Add(10.1200, 11.3300);
-            //Assert
-            Assert.That(result, Is.EqualTo(21.45));
-        }
-
-        [TestCase(1,2,3)]
-        [TestCase(3, 3, 6)]
-        [TestCase(11.11, 11.11, 22.22)]
-        public void Add_Test3cases_SumIsCorrect(double n, double d, double q)
-        {
-            var result = uut.Add(n, d);
-            Assert.That(q,Is.EqualTo(result));
-        }
-
-        [TestCase(4, 2, 2)]
-        [TestCase(-4,-3,-1)]
-        [TestCase(-2,5,-7)]
-        public void Subtract_Test3Cases_SumIsCorrect(double a, double b, double result)
-        {
-            Assert.That(uut.Subtract(a,b) ,Is.EqualTo(result));
-        }
-
         [TestCase(2, 0)]
-        [TestCase(-3, 5)]
-        [TestCase(5, -3)]
-        public void Subtract_Overloaded_SumIsCorrect(double b, double result)
+        [TestCase(5, 0)]
+        [TestCase(0, 0)]
+        public void Multiply_overloadedFunction_SumIsCorrect(double a, double result)
         {
-            uut.Subtract(4, 2);
-            Assert.That(uut.Subtract(b), Is.EqualTo(result));
+            uut.Multiply(a);
+            Assert.That(uut.Multiply(a), Is.EqualTo(result));
         }
 
+
+        //*** Divide ***
         [TestCase(10, 2, 5)]
         [TestCase(0, 3, 0)]
         [TestCase(11.11, 11.11, 1)]
@@ -122,6 +156,15 @@ namespace Calculator.Test.Unit
         {
             var result = uut.Divide(n, d);
             Assert.That(q, Is.EqualTo(result));
+        }
+
+        [TestCase(2, 1)]
+        [TestCase(5, 0.4)]
+        [TestCase(-10, -0.2)]
+        public void Divide_overloadedFunction_SumIsCorrect(double b, double result)
+        {
+            uut.Divide(4, 2);
+            Assert.That(uut.Divide(b), Is.EqualTo(result));
         }
 
         [Test]
@@ -138,6 +181,8 @@ namespace Calculator.Test.Unit
             Assert.That(() => uut.Divide(10), Throws.TypeOf<DivideByZeroException>());
         }
 
+
+        //*** Clear ***
         [Test]
         public void Clear_AddAccumulator_AccumulatorZero()
         {
@@ -193,6 +238,8 @@ namespace Calculator.Test.Unit
             Assert.That(temp, Is.EqualTo(0));
         }
 
+
+        //*** Accumulator ***
         [TestCase(3, 4, 14)]
         [TestCase(3, 3, 12)]
         [TestCase(11.11, 11.11, 44.44)]
@@ -209,42 +256,6 @@ namespace Calculator.Test.Unit
         {
             uut.Multiply(a, b);
             Assert.That(uut.Accumulator + uut.Multiply(a,b),Is.EqualTo(result));
-        }
-
-        [TestCase(2, 4)]
-        [TestCase(5, 10)]
-        [TestCase(0, 0)]
-        public void Add_overloadedFunction_SumIsCorrect(double a, double result)
-        {
-            uut.Add(a);
-            Assert.That(uut.Add(a),Is.EqualTo(result));
-        }
-
-        [TestCase(2, -4)]
-        [TestCase(5, -10)]
-        [TestCase(0, 0)]
-        public void Subtract_overloadedFunction_SumIsCorrect(double a, double result)
-        {
-            uut.Subtract(a);
-            Assert.That(uut.Subtract(a), Is.EqualTo(result));
-        }
-
-        [TestCase(2, 0)]
-        [TestCase(5, 0)]
-        [TestCase(0, 0)]
-        public void Multiply_overloadedFunction_SumIsCorrect(double a, double result)
-        {
-            uut.Multiply(a);
-            Assert.That(uut.Multiply(a), Is.EqualTo(result));
-        }
-
-        [TestCase(2, 1)]
-        [TestCase(5, 0.4)]
-        [TestCase(-10, -0.2)]
-        public void Divide_overloadedFunction_SumIsCorrect(double b, double result)
-        {
-            uut.Divide(4 , 2);
-            Assert.That(uut.Divide(b), Is.EqualTo(result));
         }
     }
 }
